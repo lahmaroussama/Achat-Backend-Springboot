e81ef6d7ab4a1997deee87bd8fd2d5be50746c4b
ikbel436 <61362647+ikbel436@users.noreply.github.com>
2023-10-10 21:03:17 +0100

    Update CategorieProduitController.java

--- src/main/java/tn/esprit/rh/achat/controllers/CategorieProduitController.java
+++ src/main/java/tn/esprit/rh/achat/controllers/CategorieProduitController.java
@@ -7,7 +7,7 @@ import tn.esprit.rh.achat.entities.CategorieProduit;
 import tn.esprit.rh.achat.services.ICategorieProduitService;
 
 import java.util.List;
-
+//hh
 @RestController
 @Api(tags = "Gestion des categories Produit")
 @RequestMapping("/categorieProduit")
